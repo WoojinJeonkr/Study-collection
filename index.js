be698ada90ffc1388c1f0d9d0beaae8085d68b7d
WoojinJeonkr <wooeric1@naver.com>
2022-07-06 20:14:16 +0900

    Settings

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://<username>:<Password>@test.ipner.mongodb.net/?retryWrites=true&w=majority', {
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://<username>:<password>@test.ipner.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser:true, useUnifiedTopology: true.valueOf, useCreateIndex: true, useFindandModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => {res.send('Hello World!~안녕하세요~')})
+  app.get('/register', (req, res) => {res.send('Hello World!~안녕하세요~')})
+
+  app.post('/register',(req, res) => {
+    // 회원가입 시 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다
+
+      const user = new User(req.body)
+
+      user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+          success: true
+        })
+      })
+  })
+
 app.listen(port, () => {console.log('Example app listening on port ${port}!')})
