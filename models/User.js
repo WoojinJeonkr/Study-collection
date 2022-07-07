1df2bde23d354dba9f4eec417543dcddebb2134a
WoojinJeonkr <wooeric1@naver.com>
2022-07-07 20:11:01 +0900

    login

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt')
 const saltRounds = 10 
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -51,6 +52,30 @@ userSchema.pre('save', function( next ){
         next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword와 데이터베이스 내 암호화된 비밀번호 비교
+    bcrypt.compare(plainPassword, userSchema.this.password, function(err, isMatch){
+        if(err) return cb(err),
+            cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    
+    var user = this;
+    
+    // jsonwebtoken을 이용해서 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+    // -> 'secretToken' => user._id
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
