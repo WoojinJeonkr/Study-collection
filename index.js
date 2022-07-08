1ccb9fba4b90db352276678305c03e23b20b0d48
WoojinJeonkr <wooeric1@naver.com>
2022-07-08 17:47:55 +0900

    logout

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/api/users/register',(req, res) => {
 })
 
 // 로그인
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
   // 1. 요청된 이메일을 데이터베이스에 있는지 찾는다
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -66,7 +66,7 @@ app.post('/login', (req, res) => {
   })
 })
 
-app.post('api/users/auth', auth, (req, res) => {
+app.post('/api/users/auth', auth, (req, res) => {
   // 여기까지 middleware를 통과해 왔다는 얘기는 Authentication이 True라는 말
   res.status(200).json({
     _id: req.user._id,
@@ -79,6 +79,17 @@ app.post('api/users/auth', auth, (req, res) => {
     role: req.user.role,
     image: req.user.image
   })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id},
+    {token: "" }
+    , (err, user) => {
+      if(err) return res.json({ success: false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => {console.log('Example app listening on port ${port}!')})
