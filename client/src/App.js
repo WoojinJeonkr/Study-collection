384d40daa076d54fc5caa1bec1036ae934c170c7
WoojinJeonkr <wooeric1@naver.com>
2022-07-09 13:44:28 +0900

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
           Hello
-=======
-          Edit <code>src/App.js</code> and save to reload.
->>>>>>> 0df63e8f0fc803365292f5b6b3b41838dcc80b02
         </p>
         <a
           className="App-link"
