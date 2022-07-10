41d796b71289f7c1598f80e6e4bc6b7d3f466525
WoojinJeonkr <wooeric1@naver.com>
2022-07-10 22:28:26 +0900

    Register

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER } from '../_actions/types';
+import { LOGIN_USER, REGISTER_USER } from '../_actions/types';
 
 
 export default function (state={}, action) {
@@ -6,7 +6,9 @@ export default function (state={}, action) {
         case LOGIN_USER:
             return { ...state, loginSuccess: action.payload}
             break;
-    
+        case REGISTER_USER:
+            return { ...state, register: action.payload }
+            break;
         default:
             return state;
     }
