cfa31f7729551bad8d554babef7da3c164fbffb1
WoojinJeonkr <wooeric1@naver.com>
2023-01-01 22:18:45 +0900

    initialize project

---
+++ src/main/java/com/member/demo/MemberSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.member.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberSpringBootApplication.class, args);
+	}
+
+}
