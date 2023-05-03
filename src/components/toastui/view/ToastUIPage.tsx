d08ae0ea84058fb59eeec0897c219a493107c137
WoojinJeonkr <wooeric1@naver.com>
2023-05-03 14:52:13 +0900

    feat: add home icon

--- src/components/toastui/view/ToastUIPage.tsx
+++ src/components/toastui/view/ToastUIPage.tsx
@@ -1,12 +1,16 @@
-import { Paper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
+import { Button, Paper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import React from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import ToastUIEditorView from './ToastUIEditorView';
+import { useNavigate } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
 
 const ToastUIPage = () => {
   const [checked, setChecked] = React.useState<boolean>(false);
   const [mode, setMode] = React.useState<boolean>(false);
   const [content, setContent] = React.useState<string>('');
+
+  const navigate = useNavigate();
 
   const handleSwitch = () => {
     setChecked(!checked);
@@ -17,35 +21,44 @@ const ToastUIPage = () => {
     setContent(value);
   }
 
+  const moveToHome = () => {
+    navigate('/');
+  }
+
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <Typography variant='h3'>Toast UI Editor / Viewer</Typography>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>View Mode</TableCell>
+    <div>
+      <Button style={{ float: 'right' }} onClick={moveToHome}>
+        <HomeIcon />
+      </Button>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <Typography variant='h3'>Toast UI Editor / Viewer</Typography>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>View Mode</TableCell>
+              <TableCell>
+                <Switch
+                  checked={checked}
+                  onChange={handleSwitch}
+                  inputProps={{ 'aria-label': 'controlled' }}
+                  name={ mode ? 'Viewer' : 'Editor' }
+                />
+              </TableCell>
+            </TableRow>
+            <TableCell>{mode ? 'Toast UI Viewer' : 'Toast UI Editor'}</TableCell>
             <TableCell>
-              <Switch
-                checked={checked}
-                onChange={handleSwitch}
-                inputProps={{ 'aria-label': 'controlled' }}
-                name={ mode ? 'Viewer' : 'Editor' }
+              <ToastUIEditorView
+                content={content}
+                onChange={handleToastUIEditor}
+                mode={mode}
               />
             </TableCell>
-          </TableRow>
-          <TableCell>{mode ? 'Toast UI Viewer' : 'Toast UI Editor'}</TableCell>
-          <TableCell>
-            <ToastUIEditorView
-              content={content}
-              onChange={handleToastUIEditor}
-              mode={mode}
-            />
-          </TableCell>
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
