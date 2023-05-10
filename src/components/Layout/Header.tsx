8f605fbd76ddaaa25cfedbbbee43afd7f571a184
WoojinJeonkr <wooeric1@naver.com>
2023-05-10 23:42:25 +0900

    feat: install material ui

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <Typography variant="h1">Header</Typography>
+    </header>
+  );
+};
+
+export default Header;
